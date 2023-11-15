9d9d427f0a3430a3850fd52215d8ab6c796adff0
DejanSheki <dejan.sheki.lukic@gmail.com>
2023-11-15 04:27:03 +0100

    ...

--- space-tourism-website-main/script.js
+++ space-tourism-website-main/script.js
@@ -1,19 +1,30 @@
 const explore = document.querySelector('.oval a');
 const shadow = document.querySelector('.oval');
-const hoverLine = document.querySelectorAll('.hover-line');
+// const hoverLine = document.querySelectorAll('.hover-line');
 const menuElement = document.querySelectorAll('.menu ul li a');
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    menuElement.forEach(link => {
+        console.log(link);
+
+        if (window.location.href === link.getAttribute("href")) {
+            link.nextElementSibling.classList.add('active');
+            console.log(window.location.href);
+        }
+    });
+});
 
 menuElement.forEach(element => {
     element.addEventListener('mouseenter', (e) => {
         // console.log(e.target.nextElementSibling);
-        e.target.nextElementSibling.classList.add('active');
+        e.target.nextElementSibling.classList.add('hovered');
     })
 })
 
 menuElement.forEach(element => {
     element.addEventListener('mouseleave', (e) => {
         // console.log(e.target.nextElementSibling);
-        e.target.nextElementSibling.classList.remove('active');
+        e.target.nextElementSibling.classList.remove('hovered');
     })
 })
 
@@ -25,19 +36,3 @@ explore.addEventListener('mouseleave', () => {
     shadow.classList.remove('shadow');
 })
 
-document.addEventListener("DOMContentLoaded", function () {
-    // const links = document.querySelectorAll(".item_menu a");
-
-    menuElement.forEach(link => {
-        /*let slug = link.getAttribute("href").split("/").reverse()[0];
-        if (window.location.href.indexOf(slug) > -1) {
-          link.classList.add('active');
-        }*/
-        console.log(link);
-
-        if (window.location.href === link.getAttribute("href")) {
-            link.classList.add('active');
-            console.log(window.location.href);
-        }
-    });
-});
