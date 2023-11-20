c0369e5ba56e7c8c177f7db66a5380dfbd3d7b0a
DejanSheki <dejan.sheki.lukic@gmail.com>
2023-11-21 00:03:46 +0100

    ....

---
+++ space-tourism-website-main/script/technology.js
@@ -0,0 +1,41 @@
+const btnList = document.querySelectorAll('.technology-list ul li button');
+const technologyContent = document.querySelector('.technology-content');
+const technologyContainer = document.querySelector('.technology-container');
+
+
+async function fetchData(terminology = 0) {
+    const data = await fetch('../data.json');
+    const myData = await data.json();
+    const technologyData = await myData.technology;
+    console.log(technologyData);
+
+    technologyContent.innerHTML = '';
+    technologyContainer.innerHTML = '';
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+        <h6>THE TERMINOLOGY…</h6>
+        <h1>${technologyData[terminology].name}</h1>
+        <p>${technologyData[terminology].description}</p>
+    `;
+    technologyContent.appendChild(div);
+
+    const image = new Image();
+    image.src = technologyData[terminology].images.portrait;
+    image.alt = technologyData[terminology].name;
+
+    technologyContainer.appendChild(image);
+
+}
+
+fetchData()
+
+btnList.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        console.log(btn);
+        const terminology = e.target.value;
+
+        btn.classList.toggle('active-btn');
+        fetchData(terminology);
+    });
+});
