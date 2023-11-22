7b99080afd2291a049eb3298c35092516474af03
DejanSheki <dejan.sheki.lukic@gmail.com>
2023-11-22 01:11:05 +0100

    hamburger

--- space-tourism-website-main/script/index.js
+++ space-tourism-website-main/script/index.js
@@ -1,20 +1,19 @@
 const explore = document.querySelector('.oval a');
 const shadow = document.querySelector('.oval');
 const menuElement = document.querySelectorAll('.menu ul li a');
-// const hamburger = document.querySelectorAll('.hamburger img');
-// const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
 
-// hamburger.forEach(element => {
-//     element.addEventListener('click', () => {
-//         menu.style.display = 'flex';
-//     });
-// });
+
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger-open');
+    menu.classList.toggle('display');
+});
 
 document.addEventListener("DOMContentLoaded", (e) => {
     menuElement.forEach(link => {
-        // console.log(link);
-
-        if (window.location.href === link.getAttribute("href")) {
+        if (window.location.href === link.getAttribute('href')) {
             link.nextElementSibling.classList.add('active');
             console.log(window.location.href);
         }
