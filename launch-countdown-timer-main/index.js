91cf8a89b6c8b30028fdeb568a91f1bbca5e61f7
DejanSheki <dejan.sheki.lukic@gmail.com>
2023-11-30 23:33:41 +0100

    ...

--- launch-countdown-timer-main/index.js
+++ launch-countdown-timer-main/index.js
@@ -1,5 +1,40 @@
-const flipBottom = document.querySelector('.card-bottom');
+const card = document.querySelector('.card');
+const cardTop = document.querySelector('.card-top');
+const cardTopBack = document.querySelector('.card-top-back');
+const cardBottom = document.querySelector('.card-bottom');
+const cardTopFix = document.querySelector('.card-top-fixed');
 
-console.log(flipBottom);
+const countToDate = new Date().setHours(new Date().getHours() + 14);
+let previousTimeBetweenDates;
 
-flipBottom.classList.add('flip');
+setInterval(() => {
+    const currentDate = new Date();
+    const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000);
+    previousTimeBetweenDates = timeBetweenDates;
+    const seconds = timeBetweenDates % 60;
+
+    cardTop.firstElementChild.textContent = seconds;
+    cardTopBack.firstElementChild.textContent = seconds;
+    cardTopFix.firstElementChild.textContent = seconds;
+
+    cardBottom.firstElementChild.textContent = seconds;
+}, 1000)
+
+
+cardTop.classList.add('flip-top');
+cardTopBack.classList.add('flip-top-back');
+
+
+
+
+// cardTopBack.addEventListener('animationstart', e => {
+//     cardTop.firstElementChild.textContent = startNumber - 1
+// });
+
+// cardTopBack.addEventListener('animationstart', e => {
+//     cardBottom.firstElementChild.textContent = startNumber - 1
+// });
+
+// cardTop.addEventListener('animationstart', e => {
+//     cardTop.firstElementChild.textContent = startNumber - 1
+// });
