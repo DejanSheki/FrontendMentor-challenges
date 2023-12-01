b0d6643f6029c7d234834e325fe6900c7f03daf1
DejanSheki <dejan.sheki.lukic@gmail.com>
2023-12-01 16:15:57 +0100

    ...

--- launch-countdown-timer-main/index.js
+++ launch-countdown-timer-main/index.js
@@ -1,40 +1,52 @@
-const card = document.querySelector('.card');
-const cardTop = document.querySelector('.card-top');
-const cardTopBack = document.querySelector('.card-top-back');
-const cardBottom = document.querySelector('.card-bottom');
-const cardTopFix = document.querySelector('.card-top-fixed');
+const days = document.querySelector('.days');
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
 
-const countToDate = new Date().setHours(new Date().getHours() + 14);
-let previousTimeBetweenDates;
+
+const countDownDate = new Date().setDate(new Date().getDate() + 14);
 
 setInterval(() => {
-    const currentDate = new Date();
-    const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000);
-    previousTimeBetweenDates = timeBetweenDates;
-    const seconds = timeBetweenDates % 60;
+    const currentDate = new Date().getTime();
+    const distance = countDownDate - currentDate;
 
-    cardTop.firstElementChild.textContent = seconds;
-    cardTopBack.firstElementChild.textContent = seconds;
-    cardTopFix.firstElementChild.textContent = seconds;
-
-    cardBottom.firstElementChild.textContent = seconds;
-}, 1000)
+    let day = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hr = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var min = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var sec = Math.floor((distance % (1000 * 60)) / 1000);
 
 
-cardTop.classList.add('flip-top');
-cardTopBack.classList.add('flip-top-back');
+    days.childNodes[1].firstChild.textContent = day;
+    days.childNodes[5].firstChild.textContent = day;
+    hours.childNodes[1].firstChild.textContent = hr;
+    hours.childNodes[5].firstChild.textContent = hr;
+    minutes.childNodes[1].firstChild.textContent = min;
+    minutes.childNodes[5].firstChild.textContent = min;
 
+    seconds.childNodes[1].firstChild.textContent = sec;
+    // seconds.childNodes[1].classList.add('flip-top');
+    // seconds.childNodes[3].classList.add('flip-top-back');
+    seconds.childNodes[3].firstChild.textContent = sec;
+    seconds.childNodes[7].firstChild.textContent = sec;
+    // seconds.childNodes[9].firstChild.textContent = sec;
 
+}, 1000);
+
+console.log(seconds.childNodes);
+console.log(seconds.childNodes[5].firstChild);
 
 
 // cardTopBack.addEventListener('animationstart', e => {
 //     cardTop.firstElementChild.textContent = startNumber - 1
+//     cardTopBack.firstElementChild.textContent = startNumber - 1
+//     cardTopFix.firstElementChild.textContent = startNumber - 1
 // });
 
-// cardTopBack.addEventListener('animationstart', e => {
+// cardTopBack.addEventListener('animationend', e => {
 //     cardBottom.firstElementChild.textContent = startNumber - 1
 // });
 
-// cardTop.addEventListener('animationstart', e => {
+// cardTop.addEventListener('animatioend', e => {
 //     cardTop.firstElementChild.textContent = startNumber - 1
 // });
+
