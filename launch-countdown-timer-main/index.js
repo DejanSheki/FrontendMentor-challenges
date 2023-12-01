95b2b43df5dcd69aa71f5916e0b10226d4cec220
DejanSheki <dejan.sheki.lukic@gmail.com>
2023-12-01 18:28:42 +0100

    ...

--- launch-countdown-timer-main/index.js
+++ launch-countdown-timer-main/index.js
@@ -24,29 +24,9 @@ setInterval(() => {
     minutes.childNodes[5].firstChild.textContent = min;
 
     seconds.childNodes[1].firstChild.textContent = sec;
-    // seconds.childNodes[1].classList.add('flip-top');
-    // seconds.childNodes[3].classList.add('flip-top-back');
     seconds.childNodes[3].firstChild.textContent = sec;
-    seconds.childNodes[7].firstChild.textContent = sec;
-    // seconds.childNodes[9].firstChild.textContent = sec;
+    seconds.childNodes[5].firstChild.textContent = sec;
+    seconds.childNodes[9].firstChild.textContent = sec;
 
 }, 1000);
-
-console.log(seconds.childNodes);
-console.log(seconds.childNodes[5].firstChild);
-
-
-// cardTopBack.addEventListener('animationstart', e => {
-//     cardTop.firstElementChild.textContent = startNumber - 1
-//     cardTopBack.firstElementChild.textContent = startNumber - 1
-//     cardTopFix.firstElementChild.textContent = startNumber - 1
-// });
-
-// cardTopBack.addEventListener('animationend', e => {
-//     cardBottom.firstElementChild.textContent = startNumber - 1
-// });
-
-// cardTop.addEventListener('animatioend', e => {
-//     cardTop.firstElementChild.textContent = startNumber - 1
-// });
 
