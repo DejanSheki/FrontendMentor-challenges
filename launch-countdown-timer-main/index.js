495b7d58b331f5813ee5601f43e301847a039759
DejanSheki <dejan.sheki.lukic@gmail.com>
2023-12-07 12:51:53 +0100

    readme updating

--- launch-countdown-timer-main/index.js
+++ launch-countdown-timer-main/index.js
@@ -2,7 +2,6 @@ const days = document.querySelector('.days');
 const hours = document.querySelector('.hours');
 const minutes = document.querySelector('.minutes');
 const seconds = document.querySelector('.seconds');
-console.log(seconds.childNodes);
 
 const countDownDate = new Date().setDate(new Date().getDate() + 14);
 
@@ -48,8 +47,10 @@ setInterval(() => {
     seconds.childNodes[1].firstChild.textContent = sec;
     seconds.childNodes[1].classList.add('animation');
     setTimeout(() => {
-        seconds.childNodes[1].classList.remove('animation');
+        days.childNodes[1].classList.remove('animation');
+        hours.childNodes[1].classList.remove('animation');
         minutes.childNodes[1].classList.remove('animation');
+        seconds.childNodes[1].classList.remove('animation');
         seconds.childNodes[5].firstElementChild.textContent = sec;
         seconds.childNodes[9].firstChild.textContent = sec;
     }, 400)
@@ -71,52 +72,6 @@ minutes.childNodes[1].addEventListener('animationend', (e) => {
 hours.childNodes[1].addEventListener('animationend', (e) => {
     hours.childNodes[5].animate(flipTopBack, flipTopBackTiming);
 });
-
-
-
-// const countToDate = new Date().setHours(new Date().getHours() + 2080);
-// let previous;
-// setInterval(() => {
-//     const currentDate = new Date();
-//     const timeBetweenDates = Math.floor((countToDate - currentDate) / 1000);
-//     if (timeBetweenDates !== previous) {
-//         flipAllCards(timeBetweenDates);
-//     }
-//     previous = timeBetweenDates;
-// }, 250);
-// function flipAllCards(time) {
-// const days = Math.floor(time / (24 * 3600));
-// const hours = Math.floor((time / 3600) % 24);
-// const minutes = Math.floor((time / 60) % 60);
-// const seconds = Math.floor(time % 60);
-// const daysCard = document.querySelector('.days > .flip-card');
-// const hoursCard = document.querySelector('.hours > .flip-card');
-// const minutesCard = document.querySelector('.minutes > .flip-card');
-// const secondsCard = document.querySelector('.seconds > .flip-card');
-// flipCard(daysCard, days);
-// flipCard(hoursCard, hours);
-// flipCard(minutesCard, minutes);
-//     flipCard(secondsCard, seconds);
-// }
-// function flipCard(flipCard, time) {
-//     time = String(time).padStart(2, '0');
-//     const currentValue = seconds.childNodes[1].firstChild.innerText;
-//     if (time == currentValue) return;
-//     seconds.childNodes[1].classList.add('animation');
-//     seconds.childNodes[1].firstChild.innerText = time;
-// const bottomFlip = document.createElement('div');
-// bottomFlip.classList.add('bottom-flip');
-// bottomFlip.innerText = time;
-// const topHalf = flipCard.querySelector('.top');
-// const bottomHalf = flipCard.querySelector('.bottom');
-// topFlip.addEventListener('animationstart', () => {
-//     topHalf.innerText = time;
-// })
-// topFlip.addEventListener('animationend', () => {
-//     topFlip.remove()
-// });
-// bottomFlip.addEventListener('animationend', () => {
-//     bottomHalf.innerText = time;
-//     bottomFlip.remove()
-// });
-// }
+days.childNodes[1].addEventListener('animationend', (e) => {
+    days.childNodes[5].animate(flipTopBack, flipTopBackTiming);
+});
